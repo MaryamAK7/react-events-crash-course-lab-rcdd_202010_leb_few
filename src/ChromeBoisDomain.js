71b13d7f9cc6e0b8e37b12d0845a6f67bd0c7a04
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-22 17:56:47 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -37,7 +37,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onClick={this.handleClick}
-        onk
+        onKeyDown={}
         width='900'
         height='600'
         tabIndex="0">
